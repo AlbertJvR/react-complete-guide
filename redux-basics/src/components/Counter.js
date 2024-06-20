b5a252a0153f1fa8232a3798d080a9f82dfeb4e2
Albert Janse van Rensburg <4578622+AlbertJvR@users.noreply.github.com>
2024-06-20 19:05:35 +1200

    refactored to use redux toolkit with 1 slice

--- redux-basics/src/components/Counter.js
+++ redux-basics/src/components/Counter.js
@@ -1,8 +1,8 @@
 import classes from './Counter.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { DECREMENT, INCREASE, INCREMENT, TOGGLE } from '../store';
+import { counterActions } from '../store';
 
-const Counter = () => {
+/*const Counter = () => {
     const toggleCounterHandler = () => {
         dispatch({ type: TOGGLE })
     };
@@ -23,6 +23,43 @@ const Counter = () => {
 
     const decrementHandler = () => {
         dispatch({ type: DECREMENT });
+    }
+
+    return (
+        <main className={ classes.counter }>
+            <h1>Redux Counter</h1>
+            { show && <div className={ classes.value }>{ counter }</div> }
+            <div>
+                <button onClick={ incrementHandler }>Increment</button>
+                <button onClick={ increaseHandler }>Increase by 5</button>
+                <button onClick={ decrementHandler }>Decrement</button>
+            </div>
+            <button onClick={ toggleCounterHandler }>Toggle Counter</button>
+        </main>
+    );
+};*/
+
+const Counter = () => {
+    const toggleCounterHandler = () => {
+        dispatch(counterActions.toggleCounter());
+    };
+
+    // Automatically sets up the subscription to the store and component will be re-rendered if the state value changes
+    const counter = useSelector((state) => state.counter);
+    const show = useSelector((state) => state.showCounter);
+
+    const dispatch = useDispatch();
+
+    const incrementHandler = () => {
+        dispatch(counterActions.increment());
+    }
+
+    const increaseHandler = () => {
+        dispatch(counterActions.increase(5)); // { type: 'SOME_UNIQUE_IDENTIFIER', payload: 5 }
+    };
+
+    const decrementHandler = () => {
+        dispatch(counterActions.decrement());
     }
 
     return (
--- redux-basics/src/store/index.js
+++ redux-basics/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 export const INCREMENT = 'INCREMENT';
 export const INCREASE = 'INCREASE';
@@ -8,40 +8,68 @@ export const TOGGLE = 'TOGGLE';
 const initialState = {
     counter: 0,
     showCounter: true
-}
-
-const counterReducer = (state = initialState, action) => {
-    if (action.type === INCREMENT) {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === INCREASE) {
-        return {
-            counter: state.counter + action.payload,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === DECREMENT) {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === TOGGLE) {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter
-        }
-    }
-
-    return state;
 };
 
-const store = createStore(counterReducer);
+// With redux toolkit, you can write code that directly mutates the state, because it detects code like this and under the
+// hood will translate to the immutable approach by doing the clone thing and returning a new state with the updated values
+// Q: Is this not slower? Should I still write the immutable version?
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.counter++;
+        },
+        decrement: (state) => {
+            state.decrement--;
+        },
+        increase: (state, action) => {
+            state.counter = state.counter + action.payload;
+        },
+        toggleCounter: (state) => {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === INCREMENT) {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//
+//     if (action.type === INCREASE) {
+//         return {
+//             counter: state.counter + action.payload,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//
+//     if (action.type === DECREMENT) {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter,
+//         };
+//     }
+//
+//     if (action.type === TOGGLE) {
+//         return {
+//             showCounter: !state.showCounter,
+//             counter: state.counter
+//         }
+//     }
+//
+//     return state;
+// };
+
+// const store = createStore(counterReducer);
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;
