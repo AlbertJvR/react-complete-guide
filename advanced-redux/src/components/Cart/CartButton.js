7a5fa0a4dc179d59e2c2c4e0b223e3ab17f88c63
Albert Janse van Rensburg <4578622+AlbertJvR@users.noreply.github.com>
2024-06-21 09:52:12 +1200

    WIP: redux implementation incomplete

--- advanced-redux/src/components/Cart/CartButton.js
+++ advanced-redux/src/components/Cart/CartButton.js
@@ -1,12 +1,20 @@
 import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+    const dispatch = useDispatch();
+
+    function handleCartToggle() {
+        dispatch(uiActions.toggleCart());
+    }
+
+    return (
+        <button className={ classes.button } onClick={ handleCartToggle }>
+            <span>My Cart</span>
+            <span className={ classes.badge }>1</span>
+        </button>
+    );
 };
 
 export default CartButton;
--- advanced-redux/src/components/Shop/Products.js
+++ advanced-redux/src/components/Shop/Products.js
@@ -1,19 +1,44 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+    {
+        id: 'p1',
+        price: 6,
+        title: 'Product 1',
+        description: 'Some description about Product 1'
+    },
+    {
+        id: 'p2',
+        price: 10,
+        title: 'Product 2',
+        description: 'Some description about Product 2'
+    },
+    {
+        id: 'p3',
+        price: 70,
+        title: 'Product 3',
+        description: 'Some description about Product 3'
+    }
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={ classes.products }>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                { DUMMY_PRODUCTS.map((product) => (
+                    <ProductItem
+                        key={ product.id }
+                        id={ product.id }
+                        title={ product.title }
+                        price={ product.price }
+                        description={ product.description }
+                    />
+                )) }
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
